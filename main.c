2fa5d7326c44d547d12b48e014e53831c20d5857
邱宇航 <qyh@qyh-MacBook.lan>
2017-09-14 15:18:18 +0800

    delete userinit

--- main.c
+++ main.c
@@ -34,8 +34,6 @@ main(void)
   startothers();   // start other processors
   kinit2(P2V(4*1024*1024), P2V(PHYSTOP)); // must come after startothers()
   userinit();      // first user process
-  userinit();      // second user process
-  userinit();      // third user process
   mpmain();        // finish this processor's setup
 }
 
